e1fe12b2299bf55313480e75d0195985032d6eb6
bag3557 <bag3557@gmail.com>
2018-10-01 08:28:48 +0530

    first commit

---
+++ src/actions/users.js
@@ -0,0 +1,9 @@
+import { userLoggedIn } from "./auth"
+import api from '../api'
+
+export const signup = data => dispatch => 
+    api.user.signup(data)
+    .then(user => {
+        dispatch(userLoggedIn(user))
+        localStorage.bookwormJWT = user.token;
+    })
